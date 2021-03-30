b6effca9587012e961961a54607756c05315aa5b
Xingdong Bian <bianxingdong@gmail.com>
2021-03-30 21:13:54 +0100

    add study link in plot page

--- fieldtrial/views.py
+++ fieldtrial/views.py
@@ -23,4 +23,7 @@ def single_study(request, study_id):
     return render(request, 'fieldtrial_dynamic.html', {'data': get_study(study_id), 'type': 'Grassroots:Study'})
 
 def single_plot(request, plot_id):
-    return render(request, 'fieldtrialplots_dynamic.html', {'data': get_plot(plot_id)})
+    plot = get_plot(plot_id)
+    plot_json = json.loads(plot)
+    study_name = plot_json['results'][0]['results'][0]['data']['so:name']
+    return render(request, 'fieldtrialplots_dynamic.html', {'data': plot, 'plot_id': plot_id, 'study_name': study_name})
