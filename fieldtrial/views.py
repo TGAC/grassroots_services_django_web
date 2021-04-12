60580d414cb825d6ea355c9d93c45dc268440483
Xingdong Bian <bianxingdong@gmail.com>
2021-04-12 10:24:42 +0100

    change the html templates names

--- fieldtrial/views.py
+++ fieldtrial/views.py
@@ -11,19 +11,19 @@ from .grassroots_fieldtrial_requests import get_plot
 
 
 def index_loading(request):
-    return render(request, 'fieldtrial_dynamic_loading.html', {})
+    return render(request, 'fieldtrial_loading.html', {})
 
 def index(request):
-    return render(request, 'fieldtrial_dynamic.html', {'data': get_all_fieldtrials, 'type': 'AllFieldTrials'})
+    return render(request, 'fieldtrial.html', {'data': get_all_fieldtrials, 'type': 'AllFieldTrials'})
 
 def single_fieldtrial(request, fieldtrial_id):
-    return render(request, 'fieldtrial_dynamic.html', {'data': get_fieldtrial(fieldtrial_id), 'type': 'Grassroots:FieldTrial'})
+    return render(request, 'fieldtrial.html', {'data': get_fieldtrial(fieldtrial_id), 'type': 'Grassroots:FieldTrial'})
 
 def single_study(request, study_id):
-    return render(request, 'fieldtrial_dynamic.html', {'data': get_study(study_id), 'type': 'Grassroots:Study'})
+    return render(request, 'study.html', {'data': get_study(study_id), 'type': 'Grassroots:Study'})
 
 def single_plot(request, plot_id):
     plot = get_plot(plot_id)
     plot_json = json.loads(plot)
     study_name = plot_json['results'][0]['results'][0]['data']['so:name']
-    return render(request, 'fieldtrialplots_dynamic.html', {'data': plot, 'plot_id': plot_id, 'study_name': study_name})
+    return render(request, 'plots.html', {'data': plot, 'plot_id': plot_id, 'study_name': study_name})
