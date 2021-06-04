2171e97e1f2eb7b831edbcf41a2c84545f9bfef0
Xingdong Bian <bianxingdong@gmail.com>
2021-06-04 12:26:25 +0100

    add more doc to the field trial app

--- fieldtrial/views.py
+++ fieldtrial/views.py
@@ -10,28 +10,43 @@ from .grassroots_fieldtrial_requests import get_study
 from .grassroots_fieldtrial_requests import get_plot
 from .grassroots_fieldtrial_requests import search_fieldtrial
 
-
+'''
+Field trial index page request, pre-load the template
+'''
 def index_loading(request):
     return render(request, 'fieldtrial_loading.html', {})
 
+'''
+Field trial index page request, with all field trials
+'''
 def index(request):
     return render(request, 'fieldtrial.html', {'data': get_all_fieldtrials, 'type': 'AllFieldTrials'})
 
+'''
+One Field trial page request
+'''
 def single_fieldtrial(request, fieldtrial_id):
     return render(request, 'fieldtrial.html', {'data': get_fieldtrial(fieldtrial_id), 'type': 'Grassroots:FieldTrial'})
 
+'''
+One study page request
+'''
 def single_study(request, study_id):
     study = get_study(study_id)
-    # la = study[0]['address']['address']['location']['centre']['latitude']
-    # lo = study[0]['address']['address']['location']['centre']['longitude']
     return render(request, 'study.html', {'data': study, 'type': 'Grassroots:Study'})
 
+'''
+One study's plots page request
+'''
 def single_plot(request, plot_id):
     plot = get_plot(plot_id)
     plot_json = json.loads(plot)
     study_name = plot_json['results'][0]['results'][0]['data']['so:name']
     return render(request, 'plots.html', {'data': plot, 'plot_id': plot_id, 'study_name': study_name})
 
+'''
+Search field trial page request
+'''
 def search_fieldtrial(request):
     data = request.POST.get('search_str', False)
     response_json = search_fieldtrial(data)
