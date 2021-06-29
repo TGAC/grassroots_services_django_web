3b9d31ea2f1399443080f3a8534bf93b83e6655d
Xingdong Bian <bianxingdong@gmail.com>
2021-06-29 11:08:53 +0100

    add doc in settings.py

--- grassroots_services_django_web/settings.py
+++ grassroots_services_django_web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # manually added two apps
     'service.apps.ServiceConfig',
     'fieldtrial.apps.ServiceConfig',
     'django.contrib.admin',
