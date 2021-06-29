bd4806a754d00a8f571364cf0c95a3916a71cd0f
Xingdong Bian <bianxingdong@gmail.com>
2021-06-29 11:06:29 +0100

    add doc in service/url

--- service/urls.py
+++ service/urls.py
@@ -7,8 +7,13 @@ urlpatterns = [
     # ex: /service/
     path('', views.index, name='index'),
 
+    # get all services json
     path('ajax/get_all_services/', views.index_ajax),
+
+    # get one service json
     path('ajax/get_one_service/', views.single_service_ajax),
+
+    # for all other backend post request
     path('ajax/interact_backend/', views.interact_with_apache),
 
     path('private/', views.private_index),
@@ -23,8 +28,17 @@ urlpatterns = [
     path('queen/ajax/interact_backend/', views.queen_interact_with_apache),
     re_path(r'queen/(?P<service_alt_name>[A-z0-9-\w%]+)$', views.queen_single_service),
 
+
+    # for copo ontology lookup services
     path('api/co_ols', views.crop_ontology_search),
+
+    # for grassroots search services result with payload
     path('payload/<payload>', views.single_service_with_payload),
+
+    # for grassroots search query in url
+    # e.g. https://grassroots.tools/service/search/paragon
     path('search/<search_q>', views.single_service_search_q),
+
+    # service name e.g. /service/blast-blastn
     re_path(r'(?P<service_alt_name>[A-z0-9-\w%]+)$', views.single_service),
 ]
