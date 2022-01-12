0f270750833fbe0b9997c93d867235f999509ee6
billy <tyrrell.simon@gmail.com>
2022-01-12 18:08:52 +0000

    fixing security

--- grassroots_services_django_web/settings.py
+++ grassroots_services_django_web/settings.py
@@ -25,8 +25,10 @@ SECRET_KEY = '^ww22@n+jl(jz!hm%qm_u-k_#^jy2z(!9q$13cngt!@yr_t&b2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+  'localhost',
+  '127.0.0.1'
+]
 
 # Application definition
 
