161f5d0070a719709693a46ff457afd0dfbab02a
billy <tyrrell.simon@gmail.com>
2022-01-12 18:06:39 +0000

    turned off debug flag!

--- grassroots_services_django_web/settings.py
+++ grassroots_services_django_web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^ww22@n+jl(jz!hm%qm_u-k_#^jy2z(!9q$13cngt!@yr_t&b2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
