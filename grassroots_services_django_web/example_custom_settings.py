3c0f4a0d0b8c5e6b19658487047f33a6d4dc9b87
Daniel <92042432+DOC-MEX@users.noreply.github.com>
2022-09-28 12:23:54 +0100

    Update example_custom_settings.py

--- grassroots_services_django_web/example_custom_settings.py
+++ grassroots_services_django_web/example_custom_settings.py
@@ -3,7 +3,7 @@
 STATIC_ROOT = "/home/billy/Applications/apache/htdocs/static/"
 
 # The web address to access the static files
-STATIC_URL = '/static/'
+STATIC_URL = 'http://localhost:2000/static/'
 
 # The web address for the grassroots server to connect to
 SERVER_URL = "http://localhost:2000/grassroots/public_backend"
