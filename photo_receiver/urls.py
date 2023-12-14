c775ab937310680ccc4fd440c529446c9d635c04
Daniel <marmandu@gmail.com>
2023-12-14 15:44:44 +0000

    Adding retrieve_photo endpoint

--- photo_receiver/urls.py
+++ photo_receiver/urls.py
@@ -2,7 +2,9 @@
 
 from django.urls import path
 from .views import PhotoUploadView  # Import your view
+from .views import PhotoRetrieveView  # Import your view
 
 urlpatterns = [
     path('upload/', PhotoUploadView.as_view(), name='photo-upload'),
+    path('retrieve_photo/<str:subfolder>/<str:photo_name>/', PhotoRetrieveView.as_view(), name='retrieve-photo'),
 ]
