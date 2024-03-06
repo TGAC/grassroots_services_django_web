4f2693d7c0f42fed72dd96e6c6d3be948cdcf7e2
billy <tyrrell.simon@gmail.com>
2024-03-06 12:21:01 +0000

    attempting to add display of logged-in user

--- service/static/scripts/gi_dynamic_web.js
+++ service/static/scripts/gi_dynamic_web.js
@@ -154,11 +154,20 @@ function get_all_services_as_table() {
 						 * display their details
 						 */
 						let logged_in_user = json ['user'];
+
 						if (logged_in_user !== null) {
 							let logged_in_user_email = logged_in_user['so:email'];
+
+	            console.info("user: " + JSON.stringify(logged_in_user));
+
 							if (logged_in_user_email !== null) {
-								$('#user').html (		"Logged in as " + logged_in_user_email);					
+								$('#user').html ("Logged in as " + logged_in_user_email);					
 							}
+	          } else {
+	            console.info("no logged-in user");
+						}
+						
+						if (logged_in_user !== null) {
 						
 						}		/* if (logged_in_user !== null) */
 						
